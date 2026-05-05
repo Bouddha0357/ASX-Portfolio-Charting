c213a6cfe4fd6a8c05bb4a2107f012331c1c90de
Bouddha0357 <kevin_sparks@orange.fr>
2026-05-05 13:40:33 +1000

    Update asx_ma_app.py
    Removed KLV and VBS

--- asx_ma_app.py
+++ asx_ma_app.py
@@ -11,7 +11,7 @@ st.title("📈 Portfolio Stock Data Downloader - Closing Price, MA20 & MA50 (Las
 # -----------------------------
 # ASX Ticker List (from your screenshot)
 tickers = [
-    "AL3",  "ALC",  "HAS",  "VUL",  "KLV",  "VBS",  "AR9",  "FCL",  "IMM",  "PLY"
+    "AL3",  "ALC",  "HAS",  "VUL",  "AR9",  "FCL",  "IMM",  "PLY"
 ]
 
 # Add ".AX" to each ticker for Yahoo Finance
